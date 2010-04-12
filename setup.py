40ccb1223d331fe9812da22a2d99cd46c26e14ad
Christian Theune <ct@flyingcircus.io>
2010-04-12 16:49:22 +0200

    Downgrade version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.translation',
-      version = '0.2dev',
+      version='0.1.1dev',
       description="Multi-lingual content for Assembly CMS",
       author="Webcrew",
       author_email="web@assembly.org",
