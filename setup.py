32ee4a898f101ed376eb9e460d0cbbb2f60ee664
Christian Theune <ct@flyingcircus.io>
2010-08-06 12:13:10 +0200

    Back to development: 0.1.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.translation',
-      version='0.1.9',
+      version='0.1.10dev',
       description="Multi-lingual content for Assembly CMS",
       author="Webcrew",
       author_email="web@assembly.org",
