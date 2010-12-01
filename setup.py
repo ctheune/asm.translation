b1b2d9605add93932a58d4c5fae16e0fd49448b6
Christian Theune <ct@flyingcircus.io>
2010-12-01 10:13:14 +0100

    Preparing release 0.1.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.translation',
-      version='0.1.11dev',
+      version='0.1.11',
       description="Multi-lingual content for Assembly CMS",
       author="Webcrew",
       author_email="web@assembly.org",
