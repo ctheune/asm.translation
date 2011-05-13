95853220ccf5e6ea3a66d7cbe0208378a7f6b995
Christian Theune <ct@flyingcircus.io>
2011-05-13 16:05:30 +0300

    Preparing release 0.1.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.translation',
-      version='0.1.12dev',
+      version='0.1.12',
       description="Multi-lingual content for Assembly CMS",
       author="Webcrew",
       author_email="web@assembly.org",
